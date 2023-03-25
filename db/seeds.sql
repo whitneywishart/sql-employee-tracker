292ba573ad874bc2f42a999e12e650e00d34ea37
whitneywishart <whitneywishart@gmail.com>
2023-03-24 18:54:55 -0700

    file and dir setup

---
+++ db/seeds.sql
@@ -0,0 +1,73 @@
+INSERT INTO
+    department (department_name)
+VALUES
+    ("Accounting"),
+    ("Human Resources"),
+    ("Information Technology"),
+    ("Marketing"),
+    ("Operations"),
+    ("Research and Development"),
+    ("Sales");
+
+INSERT INTO
+    role (id, title)
+VALUES
+    (1, "Consultant"),
+    (2, "Director"),
+    (1, "Engineer"),
+    (3, "Intern"),
+    (5, "Manager");
+
+INSERT INTO
+    employee (id, first_name, last_name)
+VALUES
+    (1, "Helen", "Adams"),
+    (2, "Jennifer", "Allen"),
+    (3, "Christopher", "Anderson"),
+    (4, "Sandra", "Baker"),
+    (5, "Jason", "Brown"),
+    (6, "Deborah", "Campbell"),
+    (7, "Dorothy", "Carter"),
+    (8, "Ronald", "Clark"),
+    (9, "Sharon", "Collins"),
+    (10, "Donald", "Davis"),
+    (11, "Margaret", "Edwards"),
+    (12, "Steven", "Evans"),
+    (13, "Maria", "Garcia"),
+    (14, "Charles", "Gonzalez"),
+    (15, "Kimberly", "Green"),
+    (16, "Elizabeth", "Hall"),
+    (17, "Barbara", "Harris"),
+    (18, "Joseph", "Hernandez"),
+    (19, "Paul", "Hill"),
+    (20, "Laura", "Jackson"),
+    (21, "Michelle", "Johnson"),
+    (22, "Linda", "Jones"),
+    (23, "Brian", "King"),
+    (24, "Sarah", "Lee"),
+    (25, "Robert", "Lewis"),
+    (26, "Anthony", "Lopez"),
+    (27, "Jeff", "Martin"),
+    (28, "Carol", "Martinez"),
+    (29, "David", "Miller"),
+    (30, "Lisa", "Mitchell"),
+    (31, "Susan", "Moore"),
+    (32, "Edward", "Nelson"),
+    (33, "Richard", "Parker"),
+    (34, "Patricia", "Perez"),
+    (35, "William", "Phillips"),
+    (36, "Kevin", "Roberts"),
+    (37, "Thomas", "Robinson"),
+    (38, "Daniel", "Rodriguez"),
+    (39, "Michael", "Scott"),
+    (40, "James", "Smith"),
+    (41, "Ruth", "Taylor"),
+    (42, "John", "Thomas"),
+    (43, "George", "Thompson"),
+    (44, "Mark", "Turner"),
+    (45, "Betty", "Walker"),
+    (46, "Karen", "White"),
+    (47, "Nancy", "Williams"),
+    (48, "Kenneth", "Wilson"),
+    (49, "Mary", "Wright"),
+    (50, "Donna", "Young");
---
+++ server.js
@@ -0,0 +1,141 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'HolgFrag77,,,',
+        database: 'employees_db'
+    },
+    console.log(`Connected to the employee database.`)
+);
+
+// POST create a department
+app.post('/api/new-department', ({ body }, res) => {
+    const sql = `INSERT INTO department (name)
+    VALUES (?)`;
+    const params = [body.name];
+    // params = what the ? should stand for
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
+
+// POST create an employee
+app.post('/api/new-employee', ({ body }, res) => {
+    const sql = `INSERT INTO employee (first_name, last_name)
+      VALUES (?)`;
+    const params = [body.name];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Successfully added an employee',
+            data: body
+        });
+    });
+});
+
+// GET read all employees
+app.get('/api/employees', (req, res) => {
+    const sql = `SELECT * FROM employee`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Successfully listed employees',
+            data: rows
+        });
+    });
+});
+
+// Delete an employee
+app.delete('/api/employee/:id', (req, res) => {
+    const sql = `DELETE FROM employee WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Employee not found'
+            });
+        } else {
+            res.json({
+                message: 'Employee record deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+// // Read list of all reviews and associated movie name using LEFT JOIN
+// app.get('/api/movie-reviews', (req, res) => {
+//   const sql = `SELECT movies.movie_name AS movie, reviews.review FROM reviews LEFT JOIN movies ON reviews.movie_id = movies.id ORDER BY movies.movie_name;`;
+//   db.query(sql, (err, rows) => {
+//     if (err) {
+//       res.status(500).json({ error: err.message });
+//       return;
+//     }
+//     res.json({
+//       message: 'success',
+//       data: rows
+//     });
+//   });
+// });
+
+// // BONUS: Update review name
+// app.put('/api/review/:id', (req, res) => {
+//   const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
+//   const params = [req.body.review, req.params.id];
+
+//   db.query(sql, params, (err, result) => {
+//     if (err) {
+//       res.status(400).json({ error: err.message });
+//     } else if (!result.affectedRows) {
+//       res.json({
+//         message: 'Movie not found'
+//       });
+//     } else {
+//       res.json({
+//         message: 'success',
+//         data: req.body,
+//         changes: result.affectedRows
+//       });
+//     }
+//   });
+// });
+
+// Default response for not found
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// Express listener
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
